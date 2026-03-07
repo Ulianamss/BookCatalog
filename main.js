41c859196cb907eb863b06b4625b36c7780606ea
Uliana <85169486+Ulianamss@users.noreply.github.com>
2026-03-08 02:30:44 +0300

    added favorites logic and changed css a little bit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const button = document.getElementById("searchButton");
 const input = document.getElementById("searchInput");
 const results = document.getElementById("results");
+const favoritesContainer = document.getElementById("favorites");
 
 button.addEventListener("click", searchBooks);
 
@@ -59,13 +60,60 @@ function showBooks(books){
             <div class="bookCard">
                 <img src="${cover}" alt="Book cover">
                 <div class="infoDiv">
-                    <h3 class="title">${title}</h3>
+                    <h3 class="bookName">${title}</h3>
                     <p class="author">${author}</p>
                     <p class="year">${year}</p>
+                    <button class="favoriteButton">Add to Favorites</button>
                 </div>  
             </div>  
         `;
 
+        const favButton = div.querySelector(".favBtn");
+        favButton.addEventListener("click", () => {
+            addToFavorites(book);
+        });
+
         results.appendChild(div);
     });
 }
+
+function addToFavorites(book){
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    favorites.push(book);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    renderFavorites();
+}
+
+function renderFavorites(){
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    favoritesContainer.innerHTML = "";
+    favorites.forEach(book => {
+        const div = document.createElement("div");
+        const title = book.title;
+        const author = book.author_name ? book.author_name[0] : "Unknown author";
+        const year = book.first_publish_year ? book.first_publish_year : "Unknown year";
+        let cover;
+
+        if (book.cover_i) {
+            cover = `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`;
+        } else {
+            cover = "https://via.placeholder.com/150x220?text=No+Cover";
+        }
+        div.innerHTML = `
+            <div class="bookCard">
+                <img src="${cover}" alt="Book cover">
+                <div class="infoDiv">
+                    <h3 class="bookName">${title}</h3>
+                    <p class="author">${author}</p>
+                    <p class="year">${year}</p>
+                    <button class="favoriteButton">Add to Favorites</button>
+                </div>  
+            </div>  
+        `;
+        favoritesContainer.appendChild(div);
+    });
+}
+
+
+renderFavorites();
