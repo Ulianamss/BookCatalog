f287704c780c66146d8f11940fc209e406bf325a
Uliana <85169486+Ulianamss@users.noreply.github.com>
2026-03-07 23:31:58 +0300

    added creation of book cards with images

--- main.js
+++ main.js
@@ -18,7 +18,37 @@ async function searchBooks() {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
+        showBooks(data.docs);
     } catch (error) {
         console.error("Network error", error);
     }
+}
+
+function showBooks(books){
+    console.log("showBooks called", books);
+    results.innerHTML = "";
+
+    books.slice(0, 10).forEach(book => {
+        const div = document.createElement("div");
+
+        const title = book.title;
+        const author = book.author_name ? book.author_name[0] : "Unknown author";
+        const year = book.first_publish_year ? book.first_publish_year : "Unknown year";
+        let cover;
+
+        if (book.cover_i) {
+            cover = `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`;
+        } else {
+            cover = "https://via.placeholder.com/150x220?text=No+Cover";
+        }
+
+        div.innerHTML = `
+            <img src="${cover}" alt="Book cover">
+            <h3>${title}</h3>
+            <p>Author: ${author}</p>
+            <p>Year: ${year}</p>
+        `;
+
+        results.appendChild(div);
+    });
 }
