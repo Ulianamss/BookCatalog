74cc8ec398ebff8c11e3bfe10b46d9ed945859e2
Uliana <85169486+Ulianamss@users.noreply.github.com>
2026-03-07 22:47:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("searchButton");
+const input = document.getElementById("searchInput");
+
+button.addEventListener("click", () => {
+
+const query = input.value;
+
+console.log("Search:", query);
+
+});
