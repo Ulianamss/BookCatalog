ddf965c5b52b1b681db208d1fdeb07a09e404436
Uliana <85169486+Ulianamss@users.noreply.github.com>
2026-03-07 23:11:23 +0300

    aoi search returns data in console.log

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const button = document.getElementById("searchButton");
 const input = document.getElementById("searchInput");
+const results = document.getElementById("results");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", searchBooks);
 
-const query = input.value;
+async function searchBooks() {
 
-console.log("Search:", query);
+    const query = input.value.trim();
+    if (!query) {
+        console.log("Enter search query");
+        return;
+    }
 
-});
+    const url = `https://openlibrary.org/search.json?q=${query}`;
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Network error", error);
+    }
+}
